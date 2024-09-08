8609107f12c5e05822bde80960cbc0362e093bf1
Enzo Di Santo <enzoddisanto@gmail.com>
2024-09-08 20:15:36 -0300

    Exercicio 05 - Atividade 2

--- hello-server.go
+++ hello-server.go
@@ -6,10 +6,6 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
 	http.HandleFunc("/", helloHandler)
 
@@ -17,4 +13,8 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err)
 	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
